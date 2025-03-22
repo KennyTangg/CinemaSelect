ada35ee35052812f33c04d6fb3a4f52a33a14f88
Kenny T <kenny.t4ng@gmail.com>
2025-03-22 23:57:29 +0700

    Update Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import moviePoster1 from "../assets/movie-poster-1.jpg";
+import moviePoster2 from "../assets/movie-poster-2.jpg";
+import moviePoster3 from "../assets/movie-poster-3.jpg";
+import moviePoster4 from "../assets/movie-poster-4.jpg";
+import moviePoster5 from "../assets/movie-poster-5.jpg";
+import moviePoster6 from "../assets/movie-poster-6.jpg";
+import moviePoster7 from "../assets/movie-poster-7.jpg";
+
+const movieData = [
+  { name: moviePoster1, title: "INTERSTELLAR" },
+  { name: moviePoster2, title: "SPIDER-MAN: INTO THE SPIDER-VERSE" },
+  { name: moviePoster3, title: "AVENGERS: INFINITY WAR" },
+  { name: moviePoster4, title: "THE SHAWSHANK REDEMPTION" }
+];
 
 const Card = () => {
   return (
-    <header>
-    </header>
+    <section className="home-now-showing">
+      <h1>NOW SHOWING IN CINEMAS</h1>
+      <div>
+        {movieData.map((data, index) => (
+          <span key={index}>
+            <img src={data.name} alt={data.title} />
+            <h5>{data.title}</h5>
+          </span>
+        ))}
+      </div>
+    </section>
   )
 }
 

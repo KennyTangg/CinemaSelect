9eec7decea1b65079da29115b6add759bba1b313
Kenny T <kenny.t4ng@gmail.com>
2025-03-24 17:07:37 +0700

    Add movies and carousel to Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState} from 'react'
 import moviePoster1 from "../assets/movie-poster-1.jpg";
 import moviePoster2 from "../assets/movie-poster-2.jpg";
 import moviePoster3 from "../assets/movie-poster-3.jpg";
@@ -11,21 +11,32 @@ const movieData = [
   { name: moviePoster1, title: "INTERSTELLAR" },
   { name: moviePoster2, title: "SPIDER-MAN: INTO THE SPIDER-VERSE" },
   { name: moviePoster3, title: "AVENGERS: INFINITY WAR" },
-  { name: moviePoster4, title: "THE SHAWSHANK REDEMPTION" }
+  { name: moviePoster4, title: "THE SHAWSHANK REDEMPTION" },
+  { name: moviePoster5, title: "OPPENHEIMER" },
+  { name: moviePoster6, title: "TOP GUN: MAVERICK" },
+  { name: moviePoster7, title: "JOKER (2019)" }
 ];
-
 const Card = () => {
+  const [startIndex, setStartIndex] = useState(0);
+
+  const handleNext = () => {
+    setStartIndex((prevIndex) => (prevIndex + 1) % (movieData.length - 3));
+  };
+
+  const displayedMovies = movieData.slice(startIndex, startIndex + 4);
+
   return (
     <section className="home-now-showing">
       <h1>NOW SHOWING IN CINEMAS</h1>
       <div>
-        {movieData.map((data, index) => (
+        {displayedMovies.map((data, index) => (
           <span key={index}>
             <img src={data.name} alt={data.title} />
             <h5>{data.title}</h5>
           </span>
         ))}
       </div>
+      <button onClick={handleNext} className="w-36 rounded-4xl self-center text-xl sm:text-2xl font-extrabold bg-gray-800 py-2 sm:py-4 border-3">Next</button>
     </section>
   )
 }

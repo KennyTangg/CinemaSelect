5e24b4da1fb6b9533ce6040fefd74dbf710c1c6a
Kenny Tang <kenny.t4ng@gmail.com>
2025-03-25 21:54:29 +0700

    Add Login Component

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react';
+import registerImage from "../assets/register-background.jpg";
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Email:', email);
+        console.log('Password:', password);
+    };
+
     return (
-        <>
-        </>
+        <div className=" bg-gray-800 px-20 pb-30 pt-15 rounded-xl shadow-xl w-full max-w-md">
+            <h2 className="text-3xl font-bold text-center text-yellow-400">Login to your account</h2>
+            <p className="text-gray-400 text-sm text-center mb-10 mt-3 ">Hello, welcome back to your account</p>
+            <form onSubmit={handleSubmit}>
+            {/* Email Input */}
+            <div className="mb-4">
+                <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className="w-full px-4 py-2 mt-2 border border-gray-400 rounded-md text-sm focus:ring-2 focus:ring-yellow-400"
+                placeholder='Enter your email'
+                />
+            </div>
+
+            {/* Password Input */}
+            <div className="mb-4">
+                <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="w-full px-4 py-2 mt-2 border border-gray-400 rounded-md text-sm focus:ring-2 focus:ring-yellow-400"
+                placeholder='Enter your password'
+                />
+            </div>
+
+            {/* Remember Me and Forgot Password */}
+            <div className='flex items-center justify-between mb-10'>
+                <div className='flex items-center'>
+                    <input type="checkbox" id="checkbox"  />
+                    <label htmlFor='checkbox' className="ml-1.5 text-xs text-gray-400 hover:cursor-pointer" >Remember me</label>
+                </div>
+                <p className="text-xs text-gray-400 hover:cursor-pointer hover:underline">Forgot Password?</p>
+            </div>
+
+            {/* Submit Button */}
+            <button
+                type="submit"
+                className="w-full bg-yellow-400 text-gray-950 font-semibold py-2 rounded-md hover:bg-yellow-500 hover:cursor-pointer"
+            >
+                Log In
+            </button>
+            </form>
+      </div>
     )
 }
 
@@ -21,7 +77,8 @@ class RegisterPage extends React.Component {
 
     render(){
         return (
-            <div>
+            <div className="min-h-screen flex items-center justify-center">
+                 <img src={registerImage} alt="Movies Background" className='absolute -z-10 w-screen brightness-50'/>
                 <Login />
             </div>
         )

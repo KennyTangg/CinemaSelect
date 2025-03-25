fa4e7241921b5bcf91ae2b1e0efb44dfff9340d5
Kenny Tang <kenny.t4ng@gmail.com>
2025-03-25 15:33:02 +0700

    Initialize RegisterPage Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Card from './components/Card';
-import Hero from './components/Hero';
+
 import HomePage from './pages/HomePage';
+import RegisterPage from './pages/RegisterPage';
 
 const App = () => {
   return (
@@ -10,14 +10,12 @@ const App = () => {
       <BrowserRouter>
         <main>
           {/* Navigation */}
-          <HomePage />
-          {/* Route default
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="*" element={<h1>404: Page Not Found</h1>} />
-          </Routes> */}
+            <Route path="/login" element={<RegisterPage />} />
+            {/* <Route path="/contact" element={<ContactPage />} />
+            <Route path="*" element={<h1>404: Page Not Found</h1>} /> */}
+          </Routes>
         </main>
       </BrowserRouter>
     </>
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+const SignUp = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+class RegisterPage extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+            <div>
+                <Login />
+            </div>
+        )
+    }
+}
+
+export default RegisterPage

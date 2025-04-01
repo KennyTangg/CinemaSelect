4c5a1c6878d2e642830bad23421504b4ae4c8322
Kenny Tang <kenny.t4ng@gmail.com>
2025-04-01 21:43:50 +0700

    Add trailer to movie page

--- frontend/src/pages/MoviePage.jsx
+++ frontend/src/pages/MoviePage.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const MoviePage = () => {
+    const [movies,setMovies] = useState([]);
+    const [loading,setLoading] = useState(true);
+    const [error,setError] = useState(null);
+    
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try{
+                const response = await axios.get('/api/movies');
+                console.log("API response: ",response.data)
+                setMovies(response.data);
+                setLoading(false);
+            } catch (error){
+                console.error("Error fetching movies data: ",error);
+                setError(error.message);
+                setLoading(true);
+            }
+        };
+
+        fetchMovies();
+    }, []);
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
+
     return (
-        <>
-            <section>
-                
-            </section>
-        </>
+        <div className='min-h-screen bg-gray-900'>
+            <div className='flex justify-center bg-gray-950'>
+                <iframe 
+                    src={movies[0].trailer.replace("watch?v=", "embed/")}
+                    title={movies[0].title}
+                    className="w-full max-w-2xl h-64"
+                    allowFullScreen
+                ></iframe>
+            </div>
+        </div>
     )
 }
 
